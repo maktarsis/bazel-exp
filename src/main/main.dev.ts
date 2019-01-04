f4251c8b5eb1818b7341e167f1f0f24db7774100
maktarsis <tarsis.maksym@gmail.com>
2019-01-05 01:03:57 +0200

    add main dev file to the main folder

--- src/main/main.dev.ts
+++ src/main/main.dev.ts
@@ -1,4 +1,4 @@
 import { platformBrowser } from '@angular/platform-browser';
-import { AppModuleDevNgFactory } from './app/app.module.dev.ngfactory';
+import { AppModuleDevNgFactory } from '../app/app.module.dev.ngfactory';
 
 platformBrowser().bootstrapModuleFactory(AppModuleDevNgFactory);
