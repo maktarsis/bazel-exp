1f974b0e2f9cb72875fa36bd88bfdd67691c2b00
maktarsis <tarsis.maksym@gmail.com>
2019-01-20 22:16:47 +0200

    refactor(app): replace loader, rename some stuff

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,31 @@
+import { NgModule, NgModuleFactoryLoader } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { NgModule } from '@angular/core';
 
-export const helloModule = '../features/hello-world/hello-world.module#HelloWorldModule';
-export const todosModule = '../features/todos/todos.module#TodosModule';
+import { HelloWorldModuleNgFactory } from '../features/hello-world/hello-world.module.ngfactory';
+import { TodosModuleNgFactory } from '../features/todos/todos.module.ngfactory';
+
+const modules = {
+  helloWorld: '../features/hello-world/hello-world.module#HelloWorldModule',
+  todos: '../features/todos/todos.module#TodosModule'
+};
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', loadChildren: helloModule },
-  { path: 'todos', pathMatch: 'full', loadChildren: todosModule }
+  { path: '', pathMatch: 'full', loadChildren: modules.helloWorld },
+  { path: 'todos', pathMatch: 'full', loadChildren: modules.todos }
 ];
+
+export class FeatureLoader extends NgModuleFactoryLoader {
+  public load(id: string) {
+    switch (id) {
+      case modules.helloWorld:
+        return Promise.resolve(HelloWorldModuleNgFactory);
+      case modules.todos:
+        return Promise.resolve(TodosModuleNgFactory);
+      default:
+        throw new Error(`Unrecognized route id ${id}`);
+    }
+  }
+}
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.dev.ts
+++ src/app/app.module.dev.ts
@@ -1,28 +1,13 @@
 import { NgModule, NgModuleFactoryLoader } from '@angular/core';
 
-import * as routes from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppModule } from './app.module';
-import { HelloWorldModuleNgFactory } from '../features/hello-world/hello-world.module.ngfactory';
-import { TodosModuleNgFactory } from '../features/todos/todos.module.ngfactory';
-
-export class MyLoader extends NgModuleFactoryLoader {
-  load(id: string) {
-    switch (id) {
-      case routes.helloModule:
-        return Promise.resolve(HelloWorldModuleNgFactory);
-      case routes.todosModule:
-        return Promise.resolve(TodosModuleNgFactory);
-      default:
-        throw new Error(`Unrecognized route id ${id}`);
-    }
-  }
-}
+import { FeatureLoader } from "./app-routing.module";
 
 @NgModule({
   imports: [AppModule],
   bootstrap: [AppComponent],
-  providers: [{ provide: NgModuleFactoryLoader, useClass: MyLoader }]
+  providers: [{ provide: NgModuleFactoryLoader, useClass: FeatureLoader }]
 })
 export class AppModuleDev {
 }
